8106f23b8ef27751c53331d173ea74829d576789
Mateusz Faderewski <kontakt@mateuszfaderewski.pl>
2026-03-30 19:20:11 +0200

    Reinforce Copy Cat joker and add audiovisual effects when voting

--- jokers/copycat.lua
+++ jokers/copycat.lua
@@ -42,8 +42,8 @@ local function create_voting_ui(state)
             n = G.UIT.R,
             config = { align = "tl" },
             nodes = {
-                { n = G.UIT.C, config = { align = "cl" },           nodes = names },
-                { n = G.UIT.C, config = { align = "cr", minw = 1 }, nodes = votes },
+                { n = G.UIT.C, config = { align = "cl" },                                     nodes = names },
+                { n = G.UIT.C, config = { align = "cr", minw = 1, id = "ttv_copycat_votes" }, nodes = votes },
             }
         } }
     }
@@ -95,7 +95,14 @@ SMODS.Joker {
         end
 
         for i = 1, 4 do
-            local joker = table.remove(pool, math.random(#pool))
+            local joker, key = pseudorandom_element(pool, "ttv_copycat")
+
+            if key then
+                pool[key] = nil
+            else
+                break
+            end
+
             voting.options[i] = {
                 card = joker,
                 name = localize({ type = "name_text", set = "Joker", key = joker }),
@@ -109,16 +116,16 @@ SMODS.Joker {
         }
         voting.votes[#voting.votes + 1] = 0
 
+        card.children.ttv_copycat_box = UIBox({
+            definition = create_voting_ui(voting),
+            config = { align = "cm", interactable = false, collideable = false, can_collide = false },
+        })
+
         if not card.config.extra then
             card.config.extra = {}
         end
 
         card.config.extra.voting = voting
-
-        card.children.ttv_copycat_box = UIBox({
-            definition = create_voting_ui(card.config.extra.voting),
-            config = { align = "cm", interactable = false, collideable = false, can_collide = false },
-        })
     end,
     remove_from_deck = function(self, card, from_debuff)
         if card.children.ttv_copycat_box then
@@ -141,10 +148,10 @@ SMODS.Joker {
         voting.time_left = string.format("%d seconds", math.floor(voting.countdown - elapsed))
 
         if elapsed >= voting.countdown then
-            SMODS.destroy_cards(card)
+            voting.done = true
 
             local max_votes = 0
-            local winner = 1
+            local winner = nil
 
             for i, votes in ipairs(voting.votes) do
                 if votes > max_votes then
@@ -153,29 +160,54 @@ SMODS.Joker {
                 end
             end
 
-            if voting.options[winner].card then
+            if winner and voting.options[winner].card then
                 SMODS.add_card({ key = voting.options[winner].card })
+            else
+                card_eval_status_text(card, "jokers", nil, nil, nil, {
+                    color = G.C.WHITE,
+                    message = "Nothing!",
+                    sound = "ttv_fart_sound1"
+                })
             end
 
-            voting.done = true
+            SMODS.destroy_cards(card, nil, true)
         end
     end,
     add_vote = function(self, card, vote, user_id)
-        if not card.config.extra.voting then
+        if not (card.config.extra and card.config.extra.voting) then
             return
         end
 
-        if vote < 1 or vote > #card.config.extra.voting.options then
+        local voting = card.config.extra.voting
+
+        if vote < 1 or vote > #voting.options then
             return
         end
 
-        if card.config.extra.voting.user_ids[user_id] then
+        if voting.user_ids[user_id] then
             return
         end
 
-        card.config.extra.voting.user_ids[user_id] = true
+        voting.user_ids[user_id] = true
 
-        local votes = card.config.extra.voting.votes[vote]
-        card.config.extra.voting.votes[vote] = votes + 1
+        voting.votes[vote] = voting.votes[vote] + 1
+
+        self:juice_up_vote(card, vote)
+    end,
+    juice_up_vote = function(self, card, i)
+        if not card.children.ttv_copycat_box then
+            return
+        end
+
+        local node = card.children.ttv_copycat_box:get_UIE_by_ID("ttv_copycat_votes")
+
+        if not node then
+            return
+        end
+
+        if node.children[i] and node.children[i].children[1] then
+            node.children[i].children[1]:juice_up()
+            play_sound("voice" .. math.random(11), 1, 0.3)
+        end
     end
 }
